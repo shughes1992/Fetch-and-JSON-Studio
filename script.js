e296b07ed7cedd1bdeacec80a76abfc545a6c6e2
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2022-04-06 09:19:51 -0400

    first req completed

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // TODO: add code here
+
+window.addEventListener("load", function () {
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+        .then(function (response) {
+            console.log(response)
+            response.json()
+                .then(function (json) {
+                    console.log(json)
+                })
+
+        })
+
+});
